3b2ccebf20e29c4323b3830009b71330ceca8c45
dritter44 <dreither44@gmail.com>
2022-06-05 15:18:29 -0500

    adding w/ saves

--- Person_class.py
+++ Person_class.py
@@ -5,4 +5,15 @@ class Person:
         self.age = age
         self.role = role
         self.password = password
-        
+
+class Staff(Person):
+    def __init__(self, name, age, role, password, employee_id):
+        super().__init__(name, age, role, password)
+        self.employee_id = employee_id
+
+class Student(Person):
+    def __init__(self, name, age, role, password, student_id):
+        super().__init__(name, age, role, password)
+        self.student_id = student_id
+
+
